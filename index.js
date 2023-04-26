04323a14834c468f9231c626040c3594f5594ff2
AnzhelikaMullova <ulanova592@gmail.com>
2023-04-26 15:51:09 +0300

    Add commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let myForms = document.forms.myForm;    //нашли форму
+let elemCarBrend = myForms.elements.carBrend;     //нашла элемент бренда автомобиля,без значения
+let elemCarModel = myForms.elements.carModel;   // элемент марки автомобиля
+let elemPetrol = myForms.elements.petrol;   //элемент инпута со значением "Бензин"
+let elemDiesel = myForms.elements.diesel;  //элемент инпута со значением "Дизель"
+let elemGas = myForms.elements.gas;        //элемент инпута со значением "Газ"
+let elemElectric = myForms.elements.electric;    //элемент инпута со значением "Электричество"
+let elemEnСapacity = myForms.elements.enСapacity;    // элемент обьема двигателя
+let elemNewCar = myForms.elements.newCar;   // элемент нового автомобиля
+let elemSuppСar = myForms.elements.suppСar;  //элемент поддержанного автомобиля
+let elemBankСard = myForms.elements.bankСard; // оплата картой
+
+let button = document.querySelector(".button"); // нашла кнопку "расчитать стоимость"
+let carOwner = document.querySelector(".carOwner"); // нашла див с количеством владельцев
+
+
+
+
+
+
+
+
+
